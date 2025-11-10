90344bdeafeb96988732c75e60658fd28510dfae
Ama Benedict Onyedikachi <benedict@engineb.com>
2025-11-10 19:47:12 +0000

    implemented authentication process

---
+++ src/users/users.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { UsersController } from './users.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { UsersService } from './users.service';
+import { AuthService } from './auth.service';
+import { CurrentUserInterceptor } from './interceptors/current-user.interceptor';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([User])],
+  controllers: [UsersController],
+  providers: [
+    UsersService,
+    AuthService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: CurrentUserInterceptor,
+    },
+  ],
+  exports: [UsersService],
+})
+export class UsersModule {}
